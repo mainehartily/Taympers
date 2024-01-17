dd56508d36be387f29a249c8f79f44ff5f841688
maimeow <126539340+mainehartily@users.noreply.github.com>
2024-01-17 18:18:51 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My Orders</a>
+            <a href="cart.php">My Cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +63 953-006-3369 </p>
+            <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+            <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJvnrVhDHNvwPNBzjWFPrxfnGCWZwrMZKggZlTsXPZhCpxGxdZKBBMCwVwJzVgtTFpTLRQV"><i class="fas fa-envelope"></i>anglmrvlla20@gmail.com</a>
+            <p> <i class="fas fa-map-marker-alt"></i>Quezon City, Philippines - 1100 </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/profile.php?id=100087050119928"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>Malikhaing Haraya</span> </div>
+
+</section>
